4498316c041de32717b7b2bc15c5a4220c9d23af
Monica Djunaidi <modjun12@gmail.com>
2020-03-04 11:44:59 -0600

    Add project files.

---
+++ Animals/Program.cs
@@ -0,0 +1,36 @@
+﻿// using system saying access to System code. Works similarly to import
+using Animals.Reptiles;
+using System;
+
+
+// namespace are bigger grouping of related classes. Bundles of classes. Coincide with folder structure
+namespace Animals
+{
+    // class is bundle of data and functionality. Can have methods on it and have pieces of information (data) attached to it. Every function in a class is a method. How programmers create projects
+    class Program
+    {
+        // below is a function, more specifically a method
+        static void Main(string[] args)
+        {
+            // creating instance to have access to Alligator class. This process is called instantiation - calls constructor of alligator and gets copy of class
+            var americanAlligator = new Alligator(100);
+            // statically defining property
+            americanAlligator.IsAquatic = true;
+
+            // different data of the same copy. Same class, different data
+            var africanAlligator = new Alligator(2);
+            africanAlligator.IsAquatic = false;
+
+            // get access to method
+            //alligator.Grunt();
+            //africanAlligator.Grunt();
+            //americanAlligator.Grunt();
+            americanAlligator.Run(75);
+            africanAlligator.Run(4);
+
+            americanAlligator.Run(40);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Animals/Reptiles/Alligator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals.Reptiles 
+{
+    // implicitly implied as internal (only accessible within this project) if not explicitly set. If want to set it accessible to all projects anywhere, use 'public' access modifier. Classes have access modifiers.
+    class Alligator
+    {
+        // storing data: property and field. Fields are generally private, never really see public. Fields are data stored just for me. If private not explicitly said within class, it will be implicitly private inside class. The underscore in the beginning is idiomatic
+        private bool _isAquatic;
+        private int _stepGoal;
+        private int _stepCount;
+        
+        // Property structure: access modifier, type of property, property name, curly braces, with auto properties get (return data) and set (set value) inside it. Properties for exposing information so can be able to modify
+        public bool IsAquatic { get; set; }
+
+        // constructors structure: accessbility modifier and name. Name of constructor matches class name. Constructors can have parameters. Constuctors if part of class is implicitly private
+        public Alligator(int stepGoal)
+        {
+            IsAquatic = true;
+            _stepGoal = stepGoal;
+        }
+
+        // Method signature structured in the following: Accessiblity modifier, method's return type, name of method, arguments in function (need to declare what type of parameter it is). If want method to return nothing, need void. "Internal" accessiblity modifier can only be called inside this project, cannot be accessed in other projects
+        public void Grunt ()
+        {
+            if (IsAquatic)
+            {
+                Console.WriteLine("Bubble Bubble");
+            }
+            else
+            {
+                Console.WriteLine("GRRRR!!");
+            }
+        }
+        public void Run(int steps)
+        {
+            _stepCount += steps;
+
+            Console.WriteLine($"You ran {steps} steps.");
+
+            if (_stepCount >= _stepGoal)
+            {
+                Console.WriteLine("Congrats, you achieved your goal");
+            }
+        }
+    }
+}
